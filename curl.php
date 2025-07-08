56eee4f5b9aabee82d17a09a6c36f90b3b89b505
maze-001 <amiecypro@gmail.com>
2025-07-08 15:28:04 +0000

    chk

---
+++ curl.php
@@ -0,0 +1,1 @@
+<?php	$ch = curl_init();	curl_setopt($ch, CURLOPT_URL, "http://www.google.com/");	curl_setopt($ch, CURLOPT_HEADER, false);	$result = curl_exec($ch);	curl_close($ch);	echo $result;	?>
