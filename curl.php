6ad8653925386389bc7937b8032bd7fdbf740b95
toha27 <tohapihi@gmail.com>
2025-07-09 16:35:25 +0530

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,7 +1,7 @@
 <?php
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://www.google.com");
-curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // store output in $result
 $result = curl_exec($ch);
 
